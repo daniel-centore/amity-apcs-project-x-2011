efdd96aabc55363a1d1e7b8ba42d1e6751fbf39b
sreservoir <sreservoir@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-05-06 00:08:22 +0000

    fine.

---
+++ Project X/src/org/amityregion5/Template.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2011 Amity AP CS A Students of 2010-2011.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation.
+ * 
+ * @author Michael Zuo <sreservoir@gmail.com>
+ */
+
+package org.amityregion5;
+
+/**
+ * A standard template for classes.
+ *
+ * The API of this class should remain constant; more methods and
+ * fields may be added, but never changed or removed.
+ *
+ * The internal implementation of this class may be significantly
+ * more ephemeral, and is subject to unforseeable change.
+ *
+ * Thus, as a rule of thumb: everything should be private unless it
+ * is documented, so as to protect yourself from relying on things
+ * that might be refactored.
+ *
+ * If you need something private, write appropriate documentation
+ * BEFORE the code itself, so that you set well-defined limits for
+ * what you're doing.
+ */
+
+public class Template {
+  // put stuff here.
+}
