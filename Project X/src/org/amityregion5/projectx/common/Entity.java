20ce589bfce2eac2a50ef727a85fa082561d311f
drdanielfrankc@gmail.com <drdanielfrankc@gmail.com@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-05-12 14:38:13 +0000

    entity

--- Project X/src/org/amityregion5/projectx/common/Entity.java
+++ Project X/src/org/amityregion5/projectx/common/Entity.java
@@ -10,12 +10,11 @@ import java.awt.Point;
  *
  * @author joe
  */
-public class Entity
-{
-   private int direction;
-   private Point location;
-   private int moveSpeed;
+public class Entity {
 
+    private int direction;
+    private Point location;
+    private int moveSpeed;
    private String name;
 
    public void test()
@@ -28,28 +27,23 @@ public class Entity
       return direction;
    }
 
-   public void setDirection(int direction)
-   {
-      this.direction = direction;
-   }
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
 
-   public Point getLocation()
-   {
-      return location;
-   }
+    public Point getLocation() {
+        return location;
+    }
 
-   public void setLocation(Point location)
-   {
-      this.location = location;
-   }
+    public void setLocation(Point location) {
+        this.location = location;
+    }
 
-   public int getMoveSpeed()
-   {
-      return moveSpeed;
-   }
+    public int getMoveSpeed() {
+        return moveSpeed;
+    }
 
-   public void setMoveSpeed(int moveSpeed)
-   {
-      this.moveSpeed = moveSpeed;
-   }
+    public void setMoveSpeed(int moveSpeed) {
+        this.moveSpeed = moveSpeed;
+    }
 }
