73553a524f44ad417a535a9ccfa2c233fafc2a5c
drdanielfrankc@gmail.com <drdanielfrankc@gmail.com@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-05-12 19:34:41 +0000

    Created Entity

---
+++ Project X/src/org/amityregion5/projectx/common/entities/Entity.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2011 Amity AP CS A Students of 2010-2011.
+ *
+ * ex: set filetype=java expandtab tabstop=4 shiftwidth=4 :
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation.
+ */
+package org.amityregion5.projectx.common.entities;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * An entity
+ * 
+ * @author Daniel Centore
+ *
+ */
+public abstract class Entity {
+
+    private int x;
+    private int y;
+    private BufferedImage image;
+    
+    public Entity(BufferedImage image, int x, int y)
+    {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public BufferedImage getImage()
+    {
+        return image;
+    }
+
+    public void setImage(BufferedImage image)
+    {
+        this.image = image;
+    }
+
+}
