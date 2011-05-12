7a5e8bf90b735ebff6c94b5e27c0c0b71c9136fa
RPGkaratemaster94 <RPGkaratemaster94@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-05-12 15:08:33 +0000

    These are all stubs for the client only classes

---
+++ Project X/src/org/amityregion5/projectx/client/main/MouseInput.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2011 Amity AP CS A Students of 2010-2011.
+ *
+ * ex: set filetype=java expandtab tabstop=4 shiftwidth=4 :
+ * * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation.
+ */
+package org.amityregion5.projectx.client.main;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+/**
+ * Class documentation.
+ *
+ * @author wenke
+ */
+public class MouseInput implements MouseMotionListener, MouseListener
+{
+
+    public void mouseDragged(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseMoved(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseClicked(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseReleased(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseEntered(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseExited(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
