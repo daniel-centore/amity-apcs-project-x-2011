15971a8f438bf62c384d6ad681df440dedd501b2
joeaarons@gmail.com <joeaarons@gmail.com@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-05-12 14:32:14 +0000

    Created an Entity skeleton... start extending boys (and girl)!

---
+++ Project X/src/org/amityregion5/projectx/common/Entity.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.amityregion5.projectx.common;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author joe
+ */
+public class Entity
+{
+   private int direction;
+   private Point location;
+   private int moveSpeed;
+
+   public int getDirection()
+   {
+      return direction;
+   }
+
+   public void setDirection(int direction)
+   {
+      this.direction = direction;
+   }
+
+   public Point getLocation()
+   {
+      return location;
+   }
+
+   public void setLocation(Point location)
+   {
+      this.location = location;
+   }
+
+   public int getMoveSpeed()
+   {
+      return moveSpeed;
+   }
+
+   public void setMoveSpeed(int moveSpeed)
+   {
+      this.moveSpeed = moveSpeed;
+   }
+}
