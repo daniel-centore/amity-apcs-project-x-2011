043fccdcec7142ee8fe51ba08825e1bc356cbb9f
RPGkaratemaster94 <RPGkaratemaster94@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-05-13 14:58:21 +0000

    These are all stubs for the client only classes

---
+++ Project X/src/org/amityregion5/projectx/client/gui/input/KeyboardInput.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2011 Amity AP CS A Students of 2010-2011.
+ *
+ * ex: set filetype=java expandtab tabstop=4 shiftwidth=4 :
+ * * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation.
+ */
+package org.amityregion5.projectx.client.gui.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Class documentation.
+ *
+ * @author wenkemi
+ */
+public class KeyboardInput implements KeyListener{
+
+    public void keyTyped(KeyEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void keyReleased(KeyEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
