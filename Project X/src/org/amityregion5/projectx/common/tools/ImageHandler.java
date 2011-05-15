076321162fe0266ca59245e52d52b566df917cc0
sreservoir <sreservoir@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-05-15 02:08:22 +0000

    wrote a rudimentary image reader

--- Project X/src/org/amityregion5/projectx/common/tools/ImageHandler.java
+++ Project X/src/org/amityregion5/projectx/common/tools/ImageHandler.java
@@ -19,6 +19,10 @@
 package org.amityregion5.projectx.common.tools;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * Class documentation.
@@ -26,10 +30,42 @@ import java.awt.image.BufferedImage;
  * @author Mike DiBuduo
  */
 public class ImageHandler {
-
+    /**
+     * Loads an image from a source specified by a string.
+     *
+     * @param str A String representing the image source.
+     * @return An image, or null if none could be found or read.
+     */
     public BufferedImage loadImage(String str)
     {
+        if (looksLikeFilePath(str))
+            return loadFile(str);
         return null;
     }
 
+    /**
+     * Determines whether a given string seems to be a file path.
+     *
+     * @param str The string to be tested.
+     * @return Whether the string should be treated as a file path.
+     */
+    private boolean looksLikeFilePath(String str) {
+        return true;
+    }
+
+    /**
+     * Loads an image from a file, given the path.
+     *
+     * @param path The filesystem path to the image.
+     * @return The image in the file, or null if reading failed.
+     */
+    public BufferedImage loadFile(String path)
+    {
+        try {
+            return ImageIO.read(new File(path));
+        }
+        catch (IOException e) {
+            return null;
+        }
+    }
 }
