c936ed43915e9945674434df7747fff216cb58e2
drdanielfrankc@gmail.com <drdanielfrankc@gmail.com@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-06-03 19:10:00 +0000

    formatting

--- Project X/src/org/amityregion5/projectx/server/game/enemies/EnemyWave.java
+++ Project X/src/org/amityregion5/projectx/server/game/enemies/EnemyWave.java
@@ -8,16 +8,17 @@ package org.amityregion5.projectx.server.game.enemies;
 import java.util.ArrayList;
 
 import org.amityregion5.projectx.common.entities.characters.enemies.Enemy;
+
 /**
  * A wave of enemies, consisting of several EnemyGroups.
+ * 
  * @author Michael Wenke
  */
-public class EnemyWave 
-{
+public class EnemyWave {
     private int waveNumber;
     private ArrayList<EnemyGroup> enemies;
     private long spawnTime;
-    private final double SPAWNTIME_DIFFICULTY_RAMP = .75;
+    private final double SPAWNTIME_DIFFICULTY_RAMP = 0.75;
     private final double NUM_ENEMIES_DIFFICULTY_RAMP = 1.5;
     private final double ENEMY_HEALTH_DIFFICULTY_RAMP = 1.5;
 
@@ -25,8 +26,8 @@ public class EnemyWave
     {
         waveNumber = n;
         enemies = en;
-        spawnTime = 500; //Random spawn time
-    }   
+        spawnTime = 500; // Random spawn time
+    }
 
     public long getSpawnTime()
     {
@@ -49,23 +50,23 @@ public class EnemyWave
     }
 
     /**
-     * Create the next wave. Will use difficulty, etc. to generate
-     * the wave.
+     * Create the next wave. Will use difficulty, etc. to generate the wave.
+     * 
      * @return the next wave to be sent
      */
     public EnemyWave nextWave()
     {
         ArrayList<EnemyGroup> newEnemies = new ArrayList<EnemyGroup>();
-        for(int i = 0; i < enemies.size(); i++)
-        {   
+        for (int i = 0; i < enemies.size(); i++)
+        {
             EnemyGroup group = enemies.get(i);
             Enemy oldEnemy = group.getEnemy();
-            Enemy newEnemy = new Enemy((int)(oldEnemy.getMaxHp()* ENEMY_HEALTH_DIFFICULTY_RAMP),0,0);
-            EnemyGroup newGroup = new EnemyGroup(newEnemy, (int)(group.getNumEnemies() * NUM_ENEMIES_DIFFICULTY_RAMP));
+            Enemy newEnemy = new Enemy((int) (oldEnemy.getMaxHp() * ENEMY_HEALTH_DIFFICULTY_RAMP), 0, 0);
+            EnemyGroup newGroup = new EnemyGroup(newEnemy, (int) (group.getNumEnemies() * NUM_ENEMIES_DIFFICULTY_RAMP));
             newEnemies.add(newGroup);
         }
-        EnemyWave nextWave = new EnemyWave(this.getWaveNumber()+1, newEnemies);
-        nextWave.setSpawnTime(this.spawnTime * (long)SPAWNTIME_DIFFICULTY_RAMP);
+        EnemyWave nextWave = new EnemyWave(this.getWaveNumber() + 1, newEnemies);
+        nextWave.setSpawnTime(this.spawnTime * (long) SPAWNTIME_DIFFICULTY_RAMP);
         return nextWave;
 
     }
