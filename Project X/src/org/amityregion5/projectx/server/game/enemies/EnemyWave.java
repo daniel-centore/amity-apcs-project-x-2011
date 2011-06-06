46785d18c2e7c432ffaf92de8630323601ee078f
sreservoir <sreservoir@2646fa89-277c-12cd-c76f-82e4bd196772>
2011-06-06 14:54:16 +0000

    polynomial increase, then.

--- Project X/src/org/amityregion5/projectx/server/game/enemies/EnemyWave.java
+++ Project X/src/org/amityregion5/projectx/server/game/enemies/EnemyWave.java
@@ -84,19 +84,19 @@ public class EnemyWave {
 
     public static double waveSpawnTime(int wn)
     {
-        //return 4 / (wn + 3); // inversely-decreasing time
-        return Math.pow(.95, wn); // exponentially-decreasing time
+        return 4 / (wn + 3); // inversely-decreasing time
+        //return Math.pow(.95, wn); // exponentially-decreasing time
     }
 
     public static double waveNumEnemies(int wn)
     {
-        //return (wn*wn + 6*wn + 9) / 16; // quadratically-increasing number
-        return Math.pow(1.2,wn); // exponentially-increasing number
+        return (wn*wn + 6*wn + 9) / 16; // quadratically-increasing number
+        //return Math.pow(1.2,wn); // exponentially-increasing number
     }
 
     public static double waveEnemyHealth(int wn) {
-        //return (wn*wn + 6*wn + 9) / 16; // quadratically-increasing health
-        return Math.pow(1.2,wn); // exponetially-increasing health
+        return (wn*wn + 6*wn + 9) / 16; // quadratically-increasing health
+        //return Math.pow(1.2,wn); // exponetially-increasing health
     }
 
 }
